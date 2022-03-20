8fdabd81f15e4788f97ade4893a9095f420582d4
Kevin Schepers <gamer.mmo@gmail.com>
2022-03-20 18:00:58 +0000

    Customize admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,31 @@
+"""All the admin settings for site products can be found here"""
 from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    """Admin settings for products"""
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """Admin settings for categories"""
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -1,8 +1,14 @@
+"""classes and functions for products models can be found here"""
 from django.db import models
 
 
 class Category(models.Model):
-    """Dunno what this is for ok..."""
+    """Create category class"""
+
+    class Meta:
+        """Edit the default plural for category"""
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -16,14 +22,14 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    """Dunno what this is for either"""
-    category = models.ForeignKey('Category', null=True, blank=True, 
+    """Create product class"""
+    category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, 
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
                                  blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
